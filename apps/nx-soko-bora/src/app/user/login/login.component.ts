7512424a47b93bcfdf7985b152e0b8adb4e799ce
owezzy <owenadira@gmail.com>
2021-05-30 14:18:59 +0300

    add comitizen

---
+++ apps/nx-soko-bora/src/app/user/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+@Component({
+  selector: 'nx-soko-bora-login',
+  template: `
+    <p>
+      login works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private store: Store) { }
+
+  ngOnInit(): void {
+  }
+
+}
